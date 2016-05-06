f2621ed81cbf930c2f939481e852dff594459f7e
mhoaglund <maxwell.hoaglund@gmail.com>
2016-05-05 23:58:51 -0500

    pulled nconf after realizing twilio doesnt need to auth for anything here.

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var nconf = require('nconf'),
-path = require('path'),
+//var nconf = require('nconf'),
+var path = require('path'),
 rita = require("rita"),
 twilio = require('twilio');
 
-nconf.argv().file({file: 'config.json'});
-global.TwilioClient = require('twilio')(nconf.get('twilioAccountKey'), nconf.get('twilioAuthToken'));
+//nconf.argv().file({file: 'config.json'});
+//global.TwilioClient = require('twilio')(nconf.get('twilioAccountKey'), nconf.get('twilioAuthToken'));
 
-var routes = require('./routes/main');
+exports.handler = function(event, context) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.say({voice:'woman'}, 'Pain,,,,,Ruin');
+    var output = '<?xml version="1.0" encoding="UTF-8"?>' + twiml.toString() + '</Response>';
+    context.succeed(output);
+};
 
 //using this for debugging responses
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  var twiml = new twilio.TwimlResponse();
+  twiml.say({voice:'woman'}, 'Pain,,,,,Ruin');
+  res.end(twiml.toString());
 }).listen(8124, "127.0.0.1");
