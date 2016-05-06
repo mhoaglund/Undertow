1456df5f155922be9fe1d1fecd84850d2512ce7b
mhoaglund <maxwell.hoaglund@gmail.com>
2016-05-06 00:04:59 -0500

    turned off dev http server

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ exports.handler = function(event, context) {
 };
 
 //using this for debugging responses
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  var twiml = new twilio.TwimlResponse();
-  twiml.say({voice:'woman'}, 'Pain,,,,,Ruin');
-  res.end(twiml.toString());
-}).listen(8124, "127.0.0.1");
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   var twiml = new twilio.TwimlResponse();
+//   twiml.say({voice:'woman'}, 'Pain,,,,,Ruin');
+//   res.end(twiml.toString());
+// }).listen(8124, "127.0.0.1");
